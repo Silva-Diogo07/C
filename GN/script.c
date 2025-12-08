c5e27f18da1314b61a58e5b1e1c09638507b6057
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2025-12-08 01:35:20 +0000

    Add code

--- GN/script.c
+++ GN/script.c
@@ -4,33 +4,75 @@
 #include <stdlib.h>
 #include <time.h>
 
+// PROTÓTIPOS (necessários porque as funções se chamam entre elas)
+int askNumber(int trys, int guess, int secret_number);
+int checkTrys(int trys, int guess, int secret_number);
+int checkNumber(int guess, int secret_number, int trys);
 
-int tentativas;
-int max_number;
-int secret_number;
-int palpite;
+int gerar_secretNumber(int max_number)
+{
+    srand(time(NULL));
+    int secret_number = rand() % (max_number + 1);
+    return secret_number;
+}
+
+int checkNumber(int guess, int secret_number, int trys)
+{
+    if (guess > secret_number)
+    {
+        printf("Mais baixo\n");
+        trys--;
+        return askNumber(trys, guess, secret_number);
+    }
+    else if (guess < secret_number)
+    {
+        printf("Mais alto\n");
+        trys--;
+        return askNumber(trys, guess, secret_number);
+    }
+
+    printf("Acertaste!\n");
+    return 0;
+}
+
+int checkTrys(int trys, int guess , int secret_number)
+{
+    if (trys > 1)
+    {
+        return checkNumber(guess, secret_number, trys);
+    }
+
+    printf("Ficaste sem tentativas!\n");
+    return 0;
+}
+
+int askNumber(int trys , int guess , int secret_number)
+{
+    guess = get_int("Qual é o teu palpite? ");
+    return checkTrys(trys, guess, secret_number);
+}
 
 int main(void)
 {
     string nome = get_string("Qual é o teu nome? ");
-    printf("Olá , %s\n", nome );
+    printf("Olá, %s!\n", nome);
 
-    char jogar = get_char("Queres jogar um jogo? (s/n): ");
+    string p1 = get_string("Queres jogar um jogo? (sim/não) ");
 
-    if (jogar == 'n')
+    if (strcmp(p1, "sim") != 0)
     {
-        printf("Fica para a próxima!\n");
+        printf("Então fica para a próxima!\n");
         return 0;
     }
 
-    tentativas = get_int("Quantas tentativas queres? ");
-    max_number = get_int("Qual será o número máximo? ");
-    
-    srand(time(NULL)); // Inicializa o gerador de números aleatorios com uma seed variável
-    secret_number = rand() % (max_number + 1); // Gera número de 0 a max_number
-   
-    palpite = get_int("Em que número estou a pensar? ");
-    printf("%i\n" , secret_number); // Debug
-    printf("%i\n" , max_number); // Debug 
-    printf("%i\n" , tentativas); // Debug
+    int trys = get_int("Quantas tentativas queres? ");
+    int max_number = get_int("Qual será o número máximo? ");
+
+    int secret = gerar_secretNumber(max_number);
+
+    printf("DEBUG → número secreto: %i\n", secret);
+
+    askNumber(trys, 0, secret);
+
+    return 0;
 }
