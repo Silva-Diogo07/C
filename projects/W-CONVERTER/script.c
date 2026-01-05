b86bf0fe271a266dda4c5d8a33ac777e10916412
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-05 22:52:57 +0000

    Finnish Weight Converter

--- projects/W-CONVERTER/script.c
+++ projects/W-CONVERTER/script.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 int main()
 
@@ -7,7 +7,30 @@ int main()
     float pounds = 0.0f;
     float kilograms = 0.0f;
 
-    printf("Weight Conversion Calculator");
+    printf("Weight Conversion Calculator\n");
+
+    printf("\n");
+    printf("Choose an option:\n");
+
+    printf("\n");
+    printf("1. Kilograms to pounds\n");
+    printf("2. Pounds to kilograms\n");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        printf("Enter how many kilograms do you want to convert: ");
+        scanf("%f", &kilograms);
+
+        pounds = kilograms * 2.20462;
+        printf("%.2f kilograms is %.2f pounds\n", kilograms, pounds);
+        return 0;
+    }
+    printf("Enter how many pounds do you want to convert: ");
+    scanf("%f", &pounds);
+
+    kilograms = pounds / 2.20462;
+    printf("%.2f pounds is %.2f kilograms\n", pounds, kilograms);
 
     return 0;
 }
