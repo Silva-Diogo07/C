345109ba1b3a9e55be0b02415ace7b187ac60e39
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-05 12:19:50 +0000

    Notes

---
+++ Notes/math.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = -3;
+
+    // x = sqrt(x);
+    // x = pow(x, 4);
+    // x = round(x);
+    // x = ceil(x);
+    // x = floor(x);
+    x = abs(x);
+
+    printf("%d\n", x);
+
+    return 0;
+}
