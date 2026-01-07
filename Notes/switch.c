33a5059e87c7230f9699c541e8006433b9662b2d
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-07 23:18:09 +0000

    Little changes

--- Notes/switch.c
+++ Notes/switch.c
@@ -8,37 +8,37 @@ int main()
 
     switch (days_of_week)
     {
-    case 1:
-        printf("It is Monday\n");
-        break;
+        case 1:
+            printf("It is Monday\n");
+            break;
 
-    case 2:
-        printf("It is Tuesday\n");
-        break;
+        case 2:
+            printf("It is Tuesday\n");
+            break;
 
-    case 3:
-        printf("It is Wednesday\n");
-        break;
+        case 3:
+            printf("It is Wednesday\n");
+            break;
 
-    case 4:
-        printf("It is Thursday\n");
-        break;
+        case 4:
+            printf("It is Thursday\n");
+            break;
 
-    case 5:
-        printf("It is Friday\n");
-        break;
+        case 5:
+            printf("It is Friday\n");
+            break;
 
-    case 6:
-        printf("It is Saturday\n");
-        break;
+        case 6:
+            printf("It is Saturday\n");
+            break;
 
-    case 7:
-        printf("It is Sunday\n");
-        break;
+        case 7:
+            printf("It is Sunday\n");
+            break;
 
-    default:
-        printf("Input not valid. Please choose a number from 1-7: ");
-        break;
+        default:
+            printf("Input not valid. Please choose a number from 1-7: ");
+            break;
     }
     
     return 0;
