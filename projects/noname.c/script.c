371c8e7be9d7db5f5499394cf15c4e40833d2242
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-08 23:06:54 +0000

    Testing

--- projects/noname.c/script.c
+++ projects/noname.c/script.c
@@ -9,6 +9,24 @@ typedef enum
     Calculators
 } programType;
 
+typedef enum
+{
+    tempConverter = 1,
+    weightConverter
+} convertersPrograms;
+
+typedef enum
+{
+    GN = 1,
+    RPS
+} gamesPrograms;
+
+typedef enum
+{
+    circleCalculator = 1,
+    compundCalculator
+} calculatorPrograms;
+
 void subMenu(programType program_type)
 {
     switch (program_type)
@@ -32,7 +50,62 @@ void subMenu(programType program_type)
             break;
 
         default:
-            printf("Invalid option. Try again:\n ");
+            printf("Invalid option. Try again.\n");
+            break;
+    }
+}
+
+void convertersProgram(convertersPrograms program)
+{
+    switch (program)
+    {
+        case tempConverter:
+            printf("Hot Food\n");
+            break;
+        
+        case weightConverter:
+            printf("Heavy Food\n");
+            break;
+
+        default:
+            printf("Invalid option. Try again.\n");
+            break;
+    }
+        
+}   
+
+void gamesProgram(gamesPrograms program)
+{
+    switch (program)
+    {
+        case GN:
+            printf("Guess the number\n");
+            break;
+
+        case RPS:
+            printf("Rock Paper Scissors\n");
+            break;
+
+        default:
+            printf("Invalid option. Try again.\n");
+            break;
+    }
+}
+
+void calculatorProgram(calculatorPrograms program)
+{
+    switch (program)
+    {
+        case circleCalculator:
+            printf("Circle Calculator Program\n");
+            break;
+
+        case compundCalculator:
+            printf("Compound Interest Calculator Program\n");
+            break;
+
+        default:
+            printf("Invalid option. Try again.\n");
             break;
     }
 }
@@ -40,6 +113,7 @@ void subMenu(programType program_type)
 int main()
 {
     int program_type = 0;
+    int program = 0;
 
     printf("Choose a type of program (1-3):\n");
 
@@ -48,7 +122,28 @@ int main()
     printf("3. Calculators\n\n");
     scanf("%d", &program_type);
     
-    switch_func(program_type);
+    subMenu(program_type);
+
+    printf("Choose a program:\n");
+    scanf("%d", &program);
+
+    if (program_type == 1)
+    {
+        convertersProgram(program);
+    }
+    else if (program_type == 2)
+    {
+        gamesProgram(program);
+    }
+    else if (program_type == 3)
+    {
+        calculatorProgram(program);
+    }
+    else
+    {
+        printf("Invalid option. Try again\n");
+        return 1;
+    }
 
     return 0;
 }
